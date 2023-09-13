093e41c8e63f24bdfb39ca712c6206c7d9cdfe2a
Ankit Jain <ankjai@microsoft.com>
2023-09-13 16:18:25 +0530

    Search Project

--- app.py
+++ app.py
@@ -1,47 +1,6 @@
-from flask import Flask, request
-
- 
-
+from flask import Flask
 app = Flask(__name__)
 
- 
-
-@app.route('/')
-
-def hello_world():
-
-    return 'Hello World'
-
- 
-
-@app.route('/post_json', methods=['POST'])
-
-def process_json():
-
-    content_type = request.headers.get('Content-Type')
-
-    print(content_type)
-
-    if (content_type == 'application/json'):
-
-        json = request.get_json()
-
- 
-
-        print(json["name"])
-
- 
-
-        return json
-
-    else:
-
-        return 'Content-Type not supported!'
-
- 
-
-# main driver function
-
-if __name__ == '__main__':
-
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "<h1>Hello World!</h1>"
